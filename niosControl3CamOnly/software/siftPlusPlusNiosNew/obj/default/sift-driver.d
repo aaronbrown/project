934d21baa5e2f3b22cd34185a39694c3fbf2ae37
Justin Horton <jkhorton@ucdavis.edu>
2013-05-29 01:41:22 -0700

    forgot to add new uart files

---
+++ niosControl3CamOnly/software/siftPlusPlusNiosNew/obj/default/sift-driver.d
@@ -0,0 +1,52 @@
+obj/default/sift-driver.o: sift-driver.cpp sift.hpp sift.ipp \
+  ../siftPlusPlusNiosNew_bsp//HAL/inc/sys/alt_timestamp.h \
+  ../siftPlusPlusNiosNew_bsp//HAL/inc/alt_types.h \
+  ../siftPlusPlusNiosNew_bsp//drivers/inc/altera_avalon_timer.h \
+  ../siftPlusPlusNiosNew_bsp//HAL/inc/alt_types.h \
+  ../siftPlusPlusNiosNew_bsp//HAL/inc/sys/alt_dev.h \
+  ../siftPlusPlusNiosNew_bsp/system.h ../siftPlusPlusNiosNew_bsp/linker.h \
+  ../siftPlusPlusNiosNew_bsp//HAL/inc/sys/alt_llist.h \
+  ../siftPlusPlusNiosNew_bsp//HAL/inc/priv/alt_dev_llist.h \
+  ../siftPlusPlusNiosNew_bsp//HAL/inc/sys/alt_llist.h \
+  ../siftPlusPlusNiosNew_bsp//HAL/inc/alt_types.h \
+  ../siftPlusPlusNiosNew_bsp//HAL/inc/sys/alt_warning.h \
+  ../siftPlusPlusNiosNew_bsp//drivers/inc/altera_avalon_pio_regs.h \
+  ../siftPlusPlusNiosNew_bsp//HAL/inc/io.h \
+  ../siftPlusPlusNiosNew_bsp//HAL/inc/alt_types.h \
+  ../siftPlusPlusNiosNew_bsp/system.h
+
+sift.hpp:
+
+sift.ipp:
+
+../siftPlusPlusNiosNew_bsp//HAL/inc/sys/alt_timestamp.h:
+
+../siftPlusPlusNiosNew_bsp//HAL/inc/alt_types.h:
+
+../siftPlusPlusNiosNew_bsp//drivers/inc/altera_avalon_timer.h:
+
+../siftPlusPlusNiosNew_bsp//HAL/inc/alt_types.h:
+
+../siftPlusPlusNiosNew_bsp//HAL/inc/sys/alt_dev.h:
+
+../siftPlusPlusNiosNew_bsp/system.h:
+
+../siftPlusPlusNiosNew_bsp/linker.h:
+
+../siftPlusPlusNiosNew_bsp//HAL/inc/sys/alt_llist.h:
+
+../siftPlusPlusNiosNew_bsp//HAL/inc/priv/alt_dev_llist.h:
+
+../siftPlusPlusNiosNew_bsp//HAL/inc/sys/alt_llist.h:
+
+../siftPlusPlusNiosNew_bsp//HAL/inc/alt_types.h:
+
+../siftPlusPlusNiosNew_bsp//HAL/inc/sys/alt_warning.h:
+
+../siftPlusPlusNiosNew_bsp//drivers/inc/altera_avalon_pio_regs.h:
+
+../siftPlusPlusNiosNew_bsp//HAL/inc/io.h:
+
+../siftPlusPlusNiosNew_bsp//HAL/inc/alt_types.h:
+
+../siftPlusPlusNiosNew_bsp/system.h:
---
+++ niosControl3CamOnly/software/siftPlusPlusNiosNew/obj/default/sift.d
@@ -0,0 +1,55 @@
+obj/default/sift.o: sift.cpp \
+  ../siftPlusPlusNiosNew_bsp//HAL/inc/sys/alt_timestamp.h \
+  ../siftPlusPlusNiosNew_bsp//HAL/inc/alt_types.h \
+  ../siftPlusPlusNiosNew_bsp//drivers/inc/altera_avalon_timer.h \
+  ../siftPlusPlusNiosNew_bsp//HAL/inc/alt_types.h \
+  ../siftPlusPlusNiosNew_bsp//HAL/inc/sys/alt_dev.h \
+  ../siftPlusPlusNiosNew_bsp/system.h ../siftPlusPlusNiosNew_bsp/linker.h \
+  ../siftPlusPlusNiosNew_bsp//HAL/inc/sys/alt_llist.h \
+  ../siftPlusPlusNiosNew_bsp//HAL/inc/priv/alt_dev_llist.h \
+  ../siftPlusPlusNiosNew_bsp//HAL/inc/sys/alt_llist.h \
+  ../siftPlusPlusNiosNew_bsp//HAL/inc/alt_types.h \
+  ../siftPlusPlusNiosNew_bsp//HAL/inc/sys/alt_warning.h sift.hpp sift.ipp \
+  sift-conv.tpp \
+  ../siftPlusPlusNiosNew_bsp//drivers/inc/altera_avalon_pio_regs.h \
+  ../siftPlusPlusNiosNew_bsp//HAL/inc/io.h \
+  ../siftPlusPlusNiosNew_bsp//HAL/inc/alt_types.h \
+  ../siftPlusPlusNiosNew_bsp/system.h
+
+../siftPlusPlusNiosNew_bsp//HAL/inc/sys/alt_timestamp.h:
+
+../siftPlusPlusNiosNew_bsp//HAL/inc/alt_types.h:
+
+../siftPlusPlusNiosNew_bsp//drivers/inc/altera_avalon_timer.h:
+
+../siftPlusPlusNiosNew_bsp//HAL/inc/alt_types.h:
+
+../siftPlusPlusNiosNew_bsp//HAL/inc/sys/alt_dev.h:
+
+../siftPlusPlusNiosNew_bsp/system.h:
+
+../siftPlusPlusNiosNew_bsp/linker.h:
+
+../siftPlusPlusNiosNew_bsp//HAL/inc/sys/alt_llist.h:
+
+../siftPlusPlusNiosNew_bsp//HAL/inc/priv/alt_dev_llist.h:
+
+../siftPlusPlusNiosNew_bsp//HAL/inc/sys/alt_llist.h:
+
+../siftPlusPlusNiosNew_bsp//HAL/inc/alt_types.h:
+
+../siftPlusPlusNiosNew_bsp//HAL/inc/sys/alt_warning.h:
+
+sift.hpp:
+
+sift.ipp:
+
+sift-conv.tpp:
+
+../siftPlusPlusNiosNew_bsp//drivers/inc/altera_avalon_pio_regs.h:
+
+../siftPlusPlusNiosNew_bsp//HAL/inc/io.h:
+
+../siftPlusPlusNiosNew_bsp//HAL/inc/alt_types.h:
+
+../siftPlusPlusNiosNew_bsp/system.h:
